0f813e9287973d882eced78573852de08d0d637c
rbhawkins999 <rbhawkins999@gmail.com>
2024-01-22 10:43:34 -0700

    Wordle1st

---
+++ Wordle.py
@@ -0,0 +1,30 @@
+# File: Wordle.py
+
+"""
+This module is the starter file for the Wordle assignment.
+BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
+"""
+
+import random
+
+from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+
+def wordle():
+
+    def enter_action(s):
+        nonlocal gw
+        
+        # Check if the entered string is a legitimate English word
+        if s.lower() in FIVE_LETTER_WORDS:
+            gw.show_message("Great! You guessed a legitimate English word.")
+        else:
+            gw.show_message("Not in word list")
+
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
+
+# Startup code
+
+if __name__ == "__main__":
+    wordle()
